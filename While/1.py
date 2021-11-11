e5ce921d4cabed26bf3eeb83fbfd65aab595e03c
Arash <53490252+ThePythonist@users.noreply.github.com>
2021-11-11 11:05:36 +0330

    Add files via upload

---
+++ While/1.py
@@ -0,0 +1,5 @@
+entry = ""
+
+while entry != "exit" :
+	entry = input("Enter exit if you want to stop : ")
+	print(entry)
---
+++ While/2.py
@@ -0,0 +1,6 @@
+flag = True
+
+while flag :
+	entry = input("Enter exit if you want to stop : ")
+	if entry == "exit" :
+		flag = False
